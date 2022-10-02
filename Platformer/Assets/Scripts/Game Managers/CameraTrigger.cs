3b532b1b28b3c61f7618e201c85a79c6ed385dae
TheJadePhoenix <114638540+TheJadePhoenix@users.noreply.github.com>
2022-10-02 00:03:36 -0400

    Changed visuals to greybox
    Changed visuals across the game for simplicity and clarity while developing

--- Platformer/Assets/Scripts/Game Managers/CameraTrigger.cs
+++ Platformer/Assets/Scripts/Game Managers/CameraTrigger.cs
@@ -9,6 +9,7 @@ public class CameraTrigger : MonoBehaviour
     CinemachineVirtualCamera cam;
     [SerializeField] int cameraNumber;
     [SerializeField] List<Text> text = new List<Text>();
+    [SerializeField] bool isTutorial = false;
 
     void Awake()
     {
@@ -22,11 +23,17 @@ public class CameraTrigger : MonoBehaviour
             if (CameraManager.activeCamera != cam)
             {
                 CameraManager.SwitchCamera(cam);
+
+                //Switches text if labeled to be a tutorial
+                if (isTutorial)
+                {
                 text[cameraNumber].enabled = true;
                 foreach (Text t in text)
                 {
                     if (t != text[cameraNumber])
                         t.enabled = false;
+                }
+
                 }
             }
         }
